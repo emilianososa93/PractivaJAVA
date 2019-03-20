b9300fa6f4876a54b26ce3fc0edbfdc4a7ce6cfa
Emiliano Sosa <emiliano_sosa93@hotmail.com>
2019-03-20 20:48:48 -0300

    pool Conexiones final

--- PoolConexiones/src/capadatos/pool/PoolConexionesOracle.java
+++ PoolConexiones/src/capadatos/pool/PoolConexionesOracle.java
@@ -4,23 +4,20 @@
  * and open the template in the editor.
  */
 package capadatos.pool;
-
 import java.sql.*;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
-
 /**
  *
  * @author Emiliano
  */
-public class PoolConexionesMySql {
-    
+public class PoolConexionesOracle {
     public static DataSource getDataSource(){
         BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUsername("root");
-        ds.setPassword("admin");
-        ds.setUrl("jdbc:mysql://localhost:3306/sga?useSSL=false");
+        ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        ds.setUsername("hr");
+        ds.setPassword("hr");
+        ds.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
         //definimos el tamaño del pool de conexines
         ds.setInitialSize(5); //definimos 5 conexiones iniciales
         return ds;
@@ -29,6 +26,4 @@ public class PoolConexionesMySql {
     public static Connection getConexion() throws SQLException{
         return getDataSource().getConnection();
     }
-    
-    
 }
--- PoolConexiones/src/poolconexiones/TestPoolConexiones.java
+++ PoolConexiones/src/poolconexiones/TestPoolConexiones.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package poolconexiones;
-
+import java.sql.*;
+import capadatos.pool.PoolConexionesMySql;
+import capadatos.pool.PoolConexionesOracle;
 /**
  *
  * @author Emiliano
@@ -15,7 +17,38 @@ public class TestPoolConexiones {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs  = null;
+        try{
+            conn = PoolConexionesMySql.getConexion();
+            System.out.println("Utilizamos el pool de conexiones de MySQL");
+            stmt = conn.prepareStatement("SELECT * FROM persona");
+            rs = stmt.executeQuery();
+            while (rs.next()){
+                System.out.print(" " + rs.getInt(1));
+                System.out.print(" " + rs.getString(2));
+                System.out.print(" " + rs.getString(3));
+                System.out.println(" ");
+            }
+            conn.close();
+            
+            conn = PoolConexionesOracle.getConexion();
+            System.out.println("Utilizamos el pool de conexiones de Oracle");
+            stmt = conn.prepareStatement("SELECT * FROM employees WHERE employee_id in (100,101,102)");
+            rs = stmt.executeQuery();
+            while (rs.next()){
+                System.out.print(" " + rs.getInt(1));
+                System.out.print(" " + rs.getString(2));
+                System.out.print(" " + rs.getString(3));
+                System.out.println(" ");
+            }
+            conn.close();
+            
+            
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
     
 }
